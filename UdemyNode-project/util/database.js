f55c1f1109ba8b1d07dabace59e371d4101a625b
matieunibre <matt.carrier183@gmail.com>
2019-04-19 11:59:39 -0700

    working

--- UdemyNode-project/util/database.js
+++ UdemyNode-project/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node', 'root', 'Burtonboy', {
+const sequelize = new Sequelize('node', '', '', {
   dialect: 'mysql',
   host: 'localhost'
 });
